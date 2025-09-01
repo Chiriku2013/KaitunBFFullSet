838e1b1c04e2a467c4e344f3f66268ff48c2d45a
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-01 16:17:45 +0700

    Update and rename KaitunFullSet.lua to KaitunBFFullSet.lua

---
+++ KaitunBFFullSet.lua
@@ -0,0 +1,44 @@
+task.spawn(function()
+    repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+    getgenv().Configs = {
+        ["Quest"] = {
+            ["Evo Race V1"] = true;
+            ["Evo Race V2"] = true;
+            ["RGB Haki"] = true;
+            ["Pull Lerver"] = true;
+        };
+        Sword = {
+            "Saber";
+            "Pole V1";
+            "Gravity Blade";
+            "Rengoku";
+            "Dragon Trident";
+            "Buddy Sword";
+            "Yama";
+            "Tushita";
+            "Cursed Dual Katana";
+        };
+        Gun = {
+            "Soul Guitar";
+            "Kabucha";
+            "Venom Bow";
+        };
+        ["FPS Booster"] = true;
+    };
+
+    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/252b811c562348a9b436d2c0d6bfae6b"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
+end)
+task.wait(1)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/DonHackRoblox/refs/heads/main/DonHackRoblox.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/TimeUi/refs/heads/main/TimeUi.lua"))()
+end)
