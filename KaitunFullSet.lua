6a8a0cc43a6ec882efcaaf0b1814cf253dff9a47
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-01 16:00:42 +0700

    Update KaitunFullSet.lua

--- KaitunFullSet.lua
+++ KaitunFullSet.lua
@@ -1,28 +1,33 @@
 task.spawn(function()
-      repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-      getgenv().Configs = {
-      ["Quest"] = {
-          ["Evo Race V1"] = true;
-          ["Evo Race V2"] = true;
-          ["RGB Haki"] = true;
-          ["Pull Lerver"] = true;
-      };
-      Sword = {
-          "Saber";
-          "Pole V1";
-          "Gravity Blade";
-          "Rengoku";
-          "Dragon Trident";
-          "Buddy Sword";
-          "Yama";
-          "Tushita";
-          "Cursed Dual Katana";
-      };
-      Gun = {
-          "Soul Guitar";
-          "Kabucha";
-          "Venom Bow";
-      };
-      ["FPS Booster"] = true;
-  };
-  loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/252b811c562348a9b436d2c0d6bfae6b"))()
+            repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Configs = {
+    ["Quest"] = {
+        ["Evo Race V1"] = true;
+        ["Evo Race V2"] = true;
+        ["RGB Haki"] = true;
+        ["Pull Lerver"] = true;
+    };
+    Sword = {
+        "Saber";
+        "Pole V1";
+        "Gravity Blade";
+        "Rengoku";
+        "Dragon Trident";
+        "Buddy Sword";
+        "Yama";
+        "Tushita";
+        "Cursed Dual Katana";
+    };
+    Gun = {
+        "Soul Guitar";
+        "Kabucha";
+        "Venom Bow";
+    };
+    ["FPS Booster"] = true;
+};
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/252b811c562348a9b436d2c0d6bfae6b"))()
+end)
+
+task.spawn(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
+end)
