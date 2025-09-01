73052f1e092a2f791524f9d1e05617f2b2046720
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-01 16:14:15 +0700

    Update KaitunFullSet.lua

--- KaitunFullSet.lua
+++ KaitunFullSet.lua
@@ -1,7 +1,7 @@
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
 end)
-task.wait()
+task.wait(1)
 
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/DonHackRoblox/refs/heads/main/DonHackRoblox.lua"))()
