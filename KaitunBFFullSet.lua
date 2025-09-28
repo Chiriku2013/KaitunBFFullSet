307bf6a1fb47fa8e7c42adeea8473018c1cbf1f3
Chiriku2013 <suzukichiriku@gmail.com>
2025-09-28 17:47:09 +0700

    Update KaitunBFFullSet.lua

--- KaitunBFFullSet.lua
+++ KaitunBFFullSet.lua
@@ -1,44 +1,38 @@
-task.spawn(function()
-    repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
-
-    getgenv().Configs = {
-        ["Quest"] = {
-            ["Evo Race V1"] = true;
-            ["Evo Race V2"] = true;
-            ["RGB Haki"] = true;
-            ["Pull Lerver"] = true;
-        };
-        Sword = {
-            "Saber";
-            "Pole V1";
-            "Gravity Blade";
-            "Rengoku";
-            "Dragon Trident";
-            "Buddy Sword";
-            "Yama";
-            "Tushita";
-            "Cursed Dual Katana";
-        };
-        Gun = {
-            "Soul Guitar";
-            "Kabucha";
-            "Venom Bow";
-        };
-        ["FPS Booster"] = true;
+getgenv().Configs = {
+    ["Quest"] = {
+        ["Evo Race V2"] = true;
+        ["Evo Race V3"] = true;
+        ["RGB Haki"] = true;
+        ["Pull Lerver"] = true;
     };
-
-    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/252b811c562348a9b436d2c0d6bfae6b"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
-end)
-task.wait(1)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/DonHackRoblox/refs/heads/main/DonHackRoblox.lua"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/TimeUi/refs/heads/main/TimeUi.lua"))()
-end)
+        ["Webhook"] = {
+        ["Url Webhook"] = "";
+        ["Ping discord"] = "";      
+        ["Time Send"] = "10m";
+    };
+    ["Sword"] = {
+        "Saber";
+        "Pole (1st Form)";
+        "Gravity Blade";
+        "Rengoku";
+        "Midnight Blade";
+        "Dragon Trident";
+        "Buddy Sword";
+        "Yama";
+        "Tushita";
+        "Cursed Dual Katana";
+    };
+    ["Gun"] = {
+        "Skull Guitar";
+        "Kabucha";
+        "Venom Bow";
+    };
+    ["FPS Booster"] = true;
+};
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shinichi-dz/phucshinsayhi/refs/heads/main/KaitunBloxFruit.lua"))()
+wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/DonHackRoblox/refs/heads/main/DonHackRoblox.lua"))()
+wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/TimeUi/refs/heads/main/TimeUi.lua"))()
