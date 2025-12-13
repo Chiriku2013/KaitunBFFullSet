45787021c22d2f167067a0e03d6f40c5418abca4
Chiriku2013 <suzukichiriku@gmail.com>
2025-12-13 17:02:23 +0700

    Update KaitunBFFullSet.lua

--- KaitunBFFullSet.lua
+++ KaitunBFFullSet.lua
@@ -1,33 +1,58 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Configs = {
-    ["Quest"] = {
-        ["Evo Race V1"] = true;
-        ["Evo Race V2"] = true;
-        ["RGB Haki"] = true;
-        ["Pull Lerver"] = true;
-    };
-    Sword = {
-        "Saber";
-        "Pole (1st Form)";
-        "Gravity Blade";
-        "Rengoku";
-        "Midnight Blade";
-        "Dragon Trident";
-        "Buddy Sword";
-        "Yama";
-        "Tushita";
-        "Cursed Dual Katana";
-    };
-    Gun = {
-        "Soul Guitar";
-        "Kabucha";
-        "Venom Bow";
-        "Dual Flintlock";
-        "Bazooka";
-    };
-    ["FPS Booster"] = false;
-};
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/8cffffd967953fe7"))()
+_G.Quest = {
+    ["RGB Aura Haki"] = true,
+    ["Pull Lever"] = true,
+    ["Quest Dough Awaken"] = true
+}
+_G.Race = {
+    ["Select Race"] = {"Human","Fishman","Skypiea","Mink"},
+    ["Lock Race"] = true,
+    ["Evo Race V3"] = true
+}
+_G.Melee = {
+    ["Godhuman"] = true
+}
+_G.Sword = {
+    ["Saber"] = true,
+    ["Midnight Blade"] = true,
+    ["Shisui"] = true,
+    ["Saddi"] = false,
+    ["Wando"] = false,
+    ["Yama"] = true,
+    ["Koko"] = false,
+    ["Rengoku"] = true,
+    ["Canvander"] = true,
+    ["Buddy Sword"] = true,
+    ["Twin Hooks"] = true,
+    ["SpikeyTrident"] = true,
+    ["Hallow Scryte"] = false,
+    ["Dark Dagger"] = false,
+    ["Tushita"] = true,
+    ["True Triple Katana"] = false,
+    ["Cursed Dual Katana"] = true,
+    ["Shark Anchor"] = false
+}
+_G.Gun = {
+    ["Kabucha"] = true,
+    ["Acidum Rifle"] = true,
+    ["Soul Guitar"] = true,
+    ["Serpent Bow"] = true
+}
+_G.Fruit = {
+    ["Main Fruit"] = {"Kitsune-Kitsune","T-Rex-T-Rex","Mammoth-Mammoth"},
+    ["Select Fruit"] = {"Dark-Dark","Magma-Magma"}
+}
+_G.Mastery = {
+    ["Melee"] = true,
+    ["Sword"] = true,
+    ["Fruit"] = true
+}
+_G.Setting = {
+    ["FPS Booster"] = true,
+    ["Safe Tween"] = false,
+    ["Speed Tween"] = 350
+}
+getgenv().Deleted_Ui = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/q8ta0e/roblox/main/SwitchHubKaitun.lua"))()
 wait()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/BFInfoUi/refs/heads/main/BFInfoUi.lua"))()
 wait(1)
